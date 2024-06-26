79bbb8cc001ee57e08b73079481e835edeafaa86
faturfawkes <nur.faturohman28@gmail.com>
2024-06-26 14:47:05 +0700

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,138 @@
+const logotext = "FATUR";
+const meta = {
+  title: "Nur Fatchurohman",
+  description:
+    "I’m Nur Fatchurohman data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+  title: "I’m Nur Fatchurohman",
+  animated: {
+    first: "Software Engineer",
+    second: "Backend Developer",
+    third: "System Designer",
+    fourth: "I build your App",
+  },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Djano",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://www.megasyariah.co.id/bms-new/edukasi-tips/wakaf_adalah.webp",
+    title: "Project Wakaf",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "/project",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "nur.faturohman28@gmail.com",
+  YOUR_FONE: "+62 821-2631-8417",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/FaturFawkes",
+  // facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/nur-fatchurohman-bab637152/",
+  twitter: "https://twitter.com",
+  instagram: "https://www.instagram.com/fatur_fawkes/",
+  whatsapp: "https://wa.me/6282126318417",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
